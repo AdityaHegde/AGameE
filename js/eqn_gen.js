9701c17364fc5301725f8cd811e001eee2bf72cf
AdityaHegde <adityahegderocks@gmail.com>
2013-07-09 10:21:02 +0530

    Checkin - 9-7-2013

--- js/eqn_gen.js
+++ js/eqn_gen.js
@@ -1,6 +1,14 @@
 (function(){
 
-  function inherit(parent, 
+  function inherit(parent, child, members) {
+    child.prototype = new parent();
+    child.prototype.constructor = child;
+    child.parent = parent;
+
+    for(var m in members) {
+      child.prototype[m] = members[m];
+    }
+  }
 
   function Base(config) {
     for(var c in config) {
@@ -10,30 +18,38 @@
 
   function EqnGen(config) {
   }
+  inherit(Base, EqnGen, {
+
+
+  });
 
   window.EqnGen = EqnGen;
 
 
   function Tokens(tokens) {
+    Tokens.parent.constructor.call(this, {});
     this.tokens = tokens;
     this.cur = 0;
   }
-  Tokens.prototype.next = function() {
-    if(this.cur >= this.tokens.length) return undefined;
-    return this.tokens[this.cur++];
-  };
-  Tokens.prototype.back = function(c) {
-    c = c || 1;
-    this.cur -= c;
-  };
+  inherit(Base, Tokens, {
+
+    next : function() {
+      if(this.cur >= this.tokens.length) return undefined;
+      return this.tokens[this.cur++];
+    },
+
+    back : function(c) {
+      c = c || 1;
+      this.cur -= c;
+    },
+  });
 
 
   function Term(config) {
-    for(var c in config) {
-      this[c] = config[c];
-    }
+    Term.parent.constructor.call(this, config);
+
     if(this.terms) {
-      
+      this.type = "bracketed";
     }
     else {
       this.var = this.var || "";
@@ -50,59 +66,105 @@
       else if(this.op === "/") {
         this.coeff = 1/this.coeff;
         this.op = "*";
+        if(this.pwr) this.pwr = -this.pwr;
       }
     }
   }
-  Term.prototype.add = function(term) {
-    if(this.var === term.var && this.pwr === term.pwr) {
-      this.coeff = (this.op === "+" ? this.coeff + term.coeff : this.coeff * term.coeff);
-      return 1;
-    }
-    return 0;
-  };
-  Term.prototype.power = function() {
-    
-  };
-  Term.prototype.simplify = function() {
-    if(this.terms) {
-      if(this.pwr) {
-        
+  inherit(Base, Term, {
+
+    equalTo : function(term) {
+      if(term.var && this.var === term.var && this.pwr === term.pwr) return 1;
+      return 0;
+    },
+
+    add : function(term) {
+      if(this.equalTo(term) === 1) {
+        this.coeff = this.coeff + term.coeff;
+        return 1;
       }
-      var ts = [], tsf = [];
-      for(var t in this.terms) {
-        var ts1 = this.terms[t].simplify();
-        for(var t1 in ts1) ts.push(ts1[t1]);
-      }
-      for(var i = 0; i < ts.length - 1; i++) {
-        if(ts[i] === 0) continue;
-        for(var j = i + 1; j < ts.length; j++) {
-          if(ts[j] === 0) continue;
-          if(ts[i].add(ts[j]) === 1) {
-            ts[j] = 0;
-          }
+      return 0;
+    },
+
+    power : function() {
+      
+    },
+
+    simplify : function() {
+      if(this.terms) {
+        if(this.pwr) {
+          
         }
-        if(ts[i].coeff !== 0) tsf.push(ts[i]);
+        var ts = [], tsf = [];
+        for(var t in this.terms) {
+          var ts1 = this.terms[t].simplify();
+          for(var t1 in ts1) ts.push(ts1[t1]);
+        }
+        for(var i = 0; i < ts.length - 1; i++) {
+          if(ts[i] === 0) continue;
+          for(var j = i + 1; j < ts.length; j++) {
+            if(ts[j] === 0) continue;
+            if(ts[i].add(ts[j]) === 1) {
+              ts[j] = 0;
+            }
+          }
+          if(ts[i].coeff !== 0) tsf.push(ts[i]);
+        }
+        if(ts[ts.length - 1] !== 0 && ts[ts.length - 1].coeff !== 0) tsf.push(ts[ts.length - 1]);
+        return tsf;
       }
-      if(ts[ts.length - 1] !== 0 && ts[ts.length - 1].coeff !== 0) tsf.push(ts[ts.length - 1]);
-      return tsf;
-    }
-    else {
-      return [this];
-    }
-  };
+      else {
+        return [this];
+      }
+    },
+
+  });
+
 
   function TermMultiply(config) {
+    TermMultiply.parent.constructor.call(this, config);
+
+    this.type = "multiTerm";
     this.terms = config.terms;
-  }
-  
-  function Eqn(config) {
-    for(var c in config) {
-      this[c] = config[c];
+    this.terms.sort(TermMultiply.sortFun);
+
+    this.coeff = 1;
+    for(var i = 0; i < this.terms.length; i++) {
+      this.coeff *= this.terms[i].coeff;
+      this.terms[i].coeff = 1;
     }
-    this.vars = {};
-    if(this.equationString) this.parseStr(this.equationString);
   }
-  window.Eqn = Eqn;
+  TermMultiply.sortFun = function(a, b) {
+    if(a === b) return 0;
+    else if(a < b) return 1;
+    else if(a > b) return -1;
+    return 0;
+  };
+  inherit(Term, TermMultiply, {
+
+    addTerm : function(term) {
+      this.terms.push(term);
+      this.terms.sort(TermMultiply.sortFun);
+      this.coeff *= term.coeff;
+      term.coeff = 1;
+    },
+
+    equalTo : function(term) {
+      if(term.terms && this.terms.length === term.terms.length) {
+        for(var i = 0; i < this.terms.length; i++) {
+          if(this.terms[i].equalTo(term.terms[i]) === 0) {
+            retrun 0;
+          }
+        }
+        return 1;
+      }
+      return 0;
+    },
+
+    simplify : function() {
+    },
+
+  });
+
 
   var operators = {
     '+' : 1,
@@ -110,79 +172,90 @@
     '*' : 1,
     '/' : 1,
   };
+  
+  function Eqn(config) {
+    Eqn.parent.constructor.call(this, config);
 
-  Eqn.prototype.parseTokens = function(tokens, coeff) {
-    var ts = [], nt, mt,
-        co = coeff,
-        t = tokens.next(),
-        op = "+";
-    while(t) {
-      if(t === ")") break;
-      if(t === "(") {
-        if(op === "-") {
-          co = -co;
+    this.vars = {};
+    if(this.equationString) this.parseStr(this.equationString);
+  }
+  window.Eqn = Eqn;
+  inherit(Base, Eqn, {
+
+    parseTokens = function(tokens, coeff) {
+      var ts = [], nt, mt,
+          co = coeff,
+          t = tokens.next(),
+          op = "+";
+      while(t) {
+        if(t === ")") break;
+        if(t === "(") {
+          if(op === "-") {
+            co = -co;
+          }
+          else if(op === "/") {
+            co = 1/co;
+          }
+          nt = new Term({terms : this.parseTokens(tokens, co)});
         }
-        else if(op === "/") {
-          co = 1/co;
+        else if(operators[t]) {
+          nt = new Term({coeff : co, op : op});
         }
-        nt = new Term({terms : this.parseTokens(tokens, co)});
-      }
-      else if(operators[t]) {
-        nt = new Term({coeff : co, op : op});
-      }
-      else {
-        if(/^(\d+)$/.test(t)) {
-          co *= t;
+        else {
+          if(/^(\d+)$/.test(t)) {
+            co *= t;
+            t = tokens.next();
+            continue;
+          }
+          this.vars[t] = 1;
+          nt = new Term({var : t, coeff : co, op : op});
+          co = coeff;
+        }
+        if(mt) {
+          mt.terms.push(nt);
+        }
+        if(!operators[t]) {
           t = tokens.next();
-          continue;
+          if(t) {
+            if(t === "^") {
+              t = tokens.next();
+              nt.pwr = Number(t);
+              t = tokens.next();
+            }
+            if(operators[t]) {
+              op = t;
+              if(op === "*" || op === "/") {
+                mt = new Term({terms : [nt]});
+              }
+              else {
+                if(mt) ts.push(mt);
+                mt = null;
+              }
+            }
+          }
         }
-        this.vars[t] = 1;
-        nt = new Term({var : t, coeff : co, op : op});
-        co = coeff;
-      }
-      if(mt) {
-        mt.terms.push(nt);
-      }
-      if(!operators[t]) {
+        
+        if(!mt) ts.push(nt);
         t = tokens.next();
-        if(t) {
-          if(t === "^") {
-            t = tokens.next();
-            nt.pwr = Number(t);
-            t = tokens.next();
-          }
-          if(operators[t]) {
-            op = t;
-            if(op === "*" || op === "/") {
-              mt = new Term({terms : [nt]});
-            }
-            else {
-              if(mt) ts.push(mt);
-              mt = null;
-            }
-          }
-        }
       }
-      
-      if(!mt) ts.push(nt);
-      t = tokens.next();
-    }
-    if(mt) ts.push(mt);
-    return ts;
-  };
+      if(mt) ts.push(mt);
+      return ts;
+    },
 
-  Eqn.prototype.parseStr = function(str) {
-    str = str.replace(/\s+/g, " ");
-    str = str.replace(/([^\s])(\(|\)|\+|-|\*|\/|\^)([^\s])/g, "$1 $2 $3");
-    str = str.replace(/(\(|\)|\+|-|\*|\/|\^)([^\s])/g, "$1 $2");
-    str = str.replace(/([^\s])(\(|\)|\+|-|\*|\/|\^)/g, "$1 $2");
+    parseStr = function(str) {
+      str = str.replace(/\s+/g, " ");
+      str = str.replace(/([^\s])(\(|\)|\+|-|\*|\/|\^)([^\s])/g, "$1 $2 $3");
+      str = str.replace(/(\(|\)|\+|-|\*|\/|\^)([^\s])/g, "$1 $2");
+      str = str.replace(/([^\s])(\(|\)|\+|-|\*|\/|\^)/g, "$1 $2");
 
-    this.equation = new Term({terms : this.parseTokens(new Tokens(str.split(" ")), 1)});
-  };
+      this.equation = new Term({terms : this.parseTokens(new Tokens(str.split(" ")), 1)});
+    },
 
-  Eqn.prototype.simplify = function() {
-    this.equation = new Term({terms : this.equation.simplify()});
-  };
+    simplify = function() {
+      this.equation = new Term({terms : this.equation.simplify()});
+    },
+
+  });
 
   EqnGen.prototype.init = function(config) {
     for(var c in config) {
